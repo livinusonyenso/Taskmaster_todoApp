01673d582b8f3483910fa71de4c6f21430ff9d55
livinusonynso <livinusonyenso@gmail.com>
2025-02-22 21:01:26 +0100

    fixed the login api bug

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser } = require("../contoller/todoController");
+const { registerUser, loginUser } = require("../contoller/todoController");
+console.log("✅ authRoutes.js loaded");
+
 
 router.post("/register", registerUser);
-
+router.post("/login", loginUser);
+router.post("/login", (req, res) => {
+    console.log("🟢 Login route hit! Request Body:", req.body);
+    loginUser(req, res);
+});
 module.exports = router;
